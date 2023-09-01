a386ee0c2478899a9f1cdfa092d2274331ed4a2e
rvcoutinho <me@rvcoutinho.com>
2023-09-01 17:38:28 -0300

    - Adding properties;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

---
+++ src/main/java/org/coldis/library/service/ServiceConfiguration.java
@@ -0,0 +1,16 @@
+package org.coldis.library.service;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Service configuration.
+ */
+@Configuration
+@PropertySource(
+		value = { "service.properties" },
+		ignoreResourceNotFound = true
+)
+public class ServiceConfiguration {
+
+}
