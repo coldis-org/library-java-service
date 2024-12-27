5f47feee7d405c3b8963dadb1e3d8bc6a695e7f0
rvcoutinho <me@rvcoutinho.com>
2024-12-27 08:41:15 -0300

    - Removing unused imports; Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/java/org/coldis/library/service/controller/JsonMapperAutoConfiguration.java
+++ src/main/java/org/coldis/library/service/controller/JsonMapperAutoConfiguration.java
@@ -14,7 +14,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
