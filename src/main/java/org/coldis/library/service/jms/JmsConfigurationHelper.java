60d399ca266b97accd2fd24e8d5f24b5d18d1f69
rvcoutinho <me@rvcoutinho.com>
2025-04-10 22:28:16 -0300

    - Tuning; Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/java/org/coldis/library/service/jms/JmsConfigurationHelper.java
+++ src/main/java/org/coldis/library/service/jms/JmsConfigurationHelper.java
@@ -47,7 +47,7 @@ public class JmsConfigurationHelper {
 
 	/** Back-off initial interval. */
 
-	@Value("${org.coldis.library.service.jms.listener.max-messages-per-task:20}")
+	@Value("${org.coldis.library.service.jms.listener.max-messages-per-task:5}")
 	private Integer maxMessagesPerTask;
 
 	/** Back-off initial interval. */
