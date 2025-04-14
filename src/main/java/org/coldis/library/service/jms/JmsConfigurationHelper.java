41f955ef823cb4cabd89452cfa738f208bed34f5
rvcoutinho <me@rvcoutinho.com>
2025-04-14 18:25:30 -0300

    - Tuning; Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/java/org/coldis/library/service/jms/JmsConfigurationHelper.java
+++ src/main/java/org/coldis/library/service/jms/JmsConfigurationHelper.java
@@ -182,7 +182,7 @@ public class JmsConfigurationHelper {
 			ActiveMQClient.injectPools(this.globalThreadPool, this.globalScheduledThreadPool, this.globalFlowControlThreadPool);
 		}
 	}
-
+	
 	/**
 	 * Gets the JMS listener executor.
 	 *
